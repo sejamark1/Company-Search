f53c41c4c132ecd1bc20a7f95b49a3526936f3a4
acemark1 <jeyachandranajinath@gmail.com>
2022-10-04 02:01:40 +0100

    Completed most of requriments
    - Able to add interest to the interest page.
    - Able to delete interest from the interest page.
    -Cleaned code.

--- app.py
+++ app.py
@@ -5,13 +5,10 @@ Companies House API and save companies of interest.
 NOT FRONT END QUERY, SHOULD BE SERVER SIDE. 
 """
 
-""" IMPORTANT
-# Activate Virtual Environment: env/Scripts/activate 
-# python app.py
-"""
 
 
 from array import array
+from curses import flash
 from email.policy import strict
 from itertools import product
 from math import prod
@@ -19,7 +16,7 @@ from operator import methodcaller
 from xml.sax.handler import all_properties
 
 from django.shortcuts import redirect
-from flask import Flask, render_template, url_for, request, jsonify 
+from flask import Flask, render_template, url_for, request, jsonify, redirect
 from flask_sqlalchemy import SQLAlchemy 
 from flask_marshmallow import Marshmallow 
 import os, json, sys, requests
@@ -58,71 +55,91 @@ class CompanySchema(ma.Schema):
 company_schema = CompanySchema() 
 companies_schema = CompanySchema(many=True)
 
+
+
+query_search_result = ""
+#RETURNS the user interest data that is stored in the database. 
+def get_interest_from_database(): 
+    all_company= CompanyIntersts.query.all() 
+    result = companies_schema.dump(all_company)
+    return result
+
+def get_search_results_from_API(query): 
+    params = {'limit':50}
+    url = "https://api.companieshouse.gov.uk/search/companies?q={}"
+    #query = str(request.form["company_search_content"])
+    api_key = "e2d9c1cf-15c9-438d-97c2-d305834265bb"
+
+    response = requests.get(url.format(query),auth=(api_key,''), params=params)
+    json_search_result = response.text
+    search_result = json.JSONDecoder().decode(json_search_result)
+    return search_result
+
+#Get the searched result and displays it here.
 @app.route('/', methods=['GET', 'POST'])
 def index(): 
     if request.method=='POST':
-        url = "https://api.companieshouse.gov.uk/search/companies?q={}"
-        query = str(request.form["company_search_content"])
-        api_key = "e2d9c1cf-15c9-438d-97c2-d305834265bb"
-
-        response = requests.get(url.format(query),auth=(api_key,''))
-        json_search_result = response.text
-        search_result = json.JSONDecoder().decode(json_search_result)
-
-        return render_template("index.html", companies = search_result['items'])
-    
-        
+        write_search_result(str(request.form["company_search_content"]))
+        query_search_result = str(request.form["company_search_content"])
+        search_result = get_search_results_from_API(query_search_result)
+        return render_template("index.html", resultCount = len(search_result['items']) , companies = search_result['items'])        
     return render_template("index.html", companies = [])
  
 
-#if request is made to (/), get method will return the following. 
-def get(): 
-    return jsonify({'msg', 'Hola '})
 
 
 
-# @app.route('/interest')
-# def view_or_remove_interest(): 
-#     return render_template("interest.html")
+def write_search_result(data):
+    with open("search.txt", "w") as searchFile: 
+        searchFile.write(data)
+
+def read_search_result():
+    with open("search.txt", "r") as searchFile: 
+        return searchFile.read()
+
+
+
 
 
 #Create a fake company and add the data to the database, not from API. 
-@app.route('/companies', methods=['POST']) 
-def add_company(): 
-    cName = request.json['company_name'] 
-    new_company = CompanyIntersts(cName)
-    db.session.add(new_company) 
-    db.session.commit()
+@app.route('/companies/<companyName>', methods=["POST", "GET"]) 
+def add_company_to_database(companyName): 
+    try:
+        new_company = CompanyIntersts(companyName)
+        db.session.add(new_company) 
+        db.session.commit()
+        query_search_result = read_search_result() #
+        search_result = get_search_results_from_API(query_search_result)
+        return render_template("index.html", resultCount = len(search_result['items']) , companies = search_result['items'])    
+    except:
+        search_result = get_search_results_from_API(query_search_result)
+        return render_template("index.html", resultCount = len(search_result['items']) , companies = search_result['items'])        
 
-    return company_schema.jsonify(new_company)
+    # return company_schema.jsonify(new_company)
 
 #Get the product, also option to delete, thus POST 
 @app.route('/interest', methods=['GET', 'POST']) 
 def get_companies(): 
-    all_company= CompanyIntersts.query.all() 
-    result = companies_schema.dump(all_company)
-    return render_template("interest.html", interests = result) 
+    return render_template("interest.html", interests = get_interest_from_database(),deleteMessage="") 
 #    return jsonify(result[0]["company_name"])
 
 
 
-
-
-#Get a single company, many be used to delete purposes. 
-@app.route('/myinterst/<id>', methods=['GET', 'POST']) 
-def get_company(id): 
-    company= CompanyIntersts.query.get(id) 
-    return company_schema.jsonify(company)
-
-
-#DELETE COMPANY 
-@app.route('/interest/<int:id>', methods=['DELETE']) 
-def delete_company(id): 
+#DELETE COMPANY from interest
+#TODO: should redirect to /interest instead /interest/id
+@app.route('/interest/<int:id>') 
+def delete_interest_from_database(id): 
     company_delete = CompanyIntersts.query.get_or_404(id) 
-    #company= CompanyIntersts.query.get(id) 
-    db.session.delete(company_delete) 
-    db.session.commit()
-    return redirect("/")
+    # #company= CompanyIntersts.query.get(id) 
+    try: 
+        db.session.delete(company_delete) 
+        db.session.commit()
+        Flask.flash(f"Sucessfully Deleted!")
+        #return Flask.redirect(url_for("interest"))
+        return render_template("interest.html", interests = get_interest_from_database(),deleteMessage="Company was deleted") 
+    except:
+        return render_template("interest.html", interests = get_interest_from_database(), deleteMessage="Company was deleted") 
+
 
 
 
